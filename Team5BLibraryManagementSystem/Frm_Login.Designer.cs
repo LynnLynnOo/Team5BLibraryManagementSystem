a2204139aa323eaa443f1a5bbb8df6d5e1047d71
Jing Wen Sam <e0321129@u.nus.edu>
2018-09-25 23:20:47 +0800

    Added logic for login form

--- Team5BLibraryManagementSystem/Frm_Login.Designer.cs
+++ Team5BLibraryManagementSystem/Frm_Login.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.label_Username = new System.Windows.Forms.Label();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.textbox_Username = new System.Windows.Forms.TextBox();
+            this.textbox_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(357, 207);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(196, 33);
+            this.btn_Login.TabIndex = 0;
+            this.btn_Login.Text = "Log In";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click_1);
+            // 
+            // label_Username
+            // 
+            this.label_Username.AutoSize = true;
+            this.label_Username.Location = new System.Drawing.Point(237, 85);
+            this.label_Username.Name = "label_Username";
+            this.label_Username.Size = new System.Drawing.Size(73, 17);
+            this.label_Username.TabIndex = 1;
+            this.label_Username.Text = "Username";
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.Location = new System.Drawing.Point(237, 122);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(69, 17);
+            this.label_Password.TabIndex = 2;
+            this.label_Password.Text = "Password";
+            // 
+            // textbox_Username
+            // 
+            this.textbox_Username.Location = new System.Drawing.Point(357, 82);
+            this.textbox_Username.Name = "textbox_Username";
+            this.textbox_Username.Size = new System.Drawing.Size(196, 22);
+            this.textbox_Username.TabIndex = 3;
+            // 
+            // textbox_Password
+            // 
+            this.textbox_Password.Location = new System.Drawing.Point(357, 122);
+            this.textbox_Password.Name = "textbox_Password";
+            this.textbox_Password.Size = new System.Drawing.Size(196, 22);
+            this.textbox_Password.TabIndex = 4;
+            this.textbox_Password.UseSystemPasswordChar = true;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Frm_Login";
+            this.Controls.Add(this.textbox_Password);
+            this.Controls.Add(this.textbox_Username);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.label_Username);
+            this.Controls.Add(this.btn_Login);
+            this.Name = "Frm_Login";
+            this.Text = "Log In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label label_Username;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.TextBox textbox_Username;
+        private System.Windows.Forms.TextBox textbox_Password;
     }
 }
