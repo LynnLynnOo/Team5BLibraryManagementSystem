8ac818a0e9defebf71c0d59da0dcb44e6e37926f
snowzinc <e0321131@u.nus.edu>
2018-09-26 09:39:56 +0800

    jiaxue

---
+++ Team5BLibraryManagementSystem/Uc-Addbooks.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Team5BLibraryManagementSystem
+{
+    partial class Uc_Addbooks
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textIsbn = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textPublisher = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textGenre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.butFind = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.butDelete = new System.Windows.Forms.Button();
+            this.butInsert = new System.Windows.Forms.Button();
+            this.butUpdate = new System.Windows.Forms.Button();
+            this.butLoad = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textAuthor = new System.Windows.Forms.TextBox();
+            this.textBookName = new System.Windows.Forms.TextBox();
+            this.textBookID = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textIsbn
+            // 
+            this.textIsbn.Location = new System.Drawing.Point(236, 185);
+            this.textIsbn.Name = "textIsbn";
+            this.textIsbn.Size = new System.Drawing.Size(100, 21);
+            this.textIsbn.TabIndex = 56;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(151, 185);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "Isbn";
+            // 
+            // textPublisher
+            // 
+            this.textPublisher.Location = new System.Drawing.Point(236, 126);
+            this.textPublisher.Name = "textPublisher";
+            this.textPublisher.Size = new System.Drawing.Size(100, 21);
+            this.textPublisher.TabIndex = 54;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Genre";
+            // 
+            // textGenre
+            // 
+            this.textGenre.Location = new System.Drawing.Point(236, 153);
+            this.textGenre.Name = "textGenre";
+            this.textGenre.Size = new System.Drawing.Size(100, 21);
+            this.textGenre.TabIndex = 52;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(151, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Publisher";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.butFind);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Location = new System.Drawing.Point(98, 296);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(367, 54);
+            this.groupBox1.TabIndex = 50;
+            this.groupBox1.TabStop = false;
+            // 
+            // butFind
+            // 
+            this.butFind.Location = new System.Drawing.Point(163, 27);
+            this.butFind.Name = "butFind";
+            this.butFind.Size = new System.Drawing.Size(75, 23);
+            this.butFind.TabIndex = 10;
+            this.butFind.Text = "Find";
+            this.butFind.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(69, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(73, 21);
+            this.textBox4.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(471, 327);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(32, 23);
+            this.button8.TabIndex = 49;
+            this.button8.Text = ">>";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(61, 323);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(31, 23);
+            this.button7.TabIndex = 48;
+            this.button7.Text = "<<";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(471, 296);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(32, 23);
+            this.button6.TabIndex = 47;
+            this.button6.Text = ">";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(61, 296);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(31, 23);
+            this.button5.TabIndex = 46;
+            this.button5.Text = "<";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // butDelete
+            // 
+            this.butDelete.Location = new System.Drawing.Point(371, 267);
+            this.butDelete.Name = "butDelete";
+            this.butDelete.Size = new System.Drawing.Size(75, 23);
+            this.butDelete.TabIndex = 45;
+            this.butDelete.Text = "Delete";
+            this.butDelete.UseVisualStyleBackColor = true;
+            // 
+            // butInsert
+            // 
+            this.butInsert.Location = new System.Drawing.Point(276, 267);
+            this.butInsert.Name = "butInsert";
+            this.butInsert.Size = new System.Drawing.Size(75, 23);
+            this.butInsert.TabIndex = 44;
+            this.butInsert.Text = "Insert";
+            this.butInsert.UseVisualStyleBackColor = true;
+            // 
+            // butUpdate
+            // 
+            this.butUpdate.Location = new System.Drawing.Point(186, 267);
+            this.butUpdate.Name = "butUpdate";
+            this.butUpdate.Size = new System.Drawing.Size(75, 23);
+            this.butUpdate.TabIndex = 43;
+            this.butUpdate.Text = "Update";
+            this.butUpdate.UseVisualStyleBackColor = true;
+            // 
+            // butLoad
+            // 
+            this.butLoad.Location = new System.Drawing.Point(105, 267);
+            this.butLoad.Name = "butLoad";
+            this.butLoad.Size = new System.Drawing.Size(75, 23);
+            this.butLoad.TabIndex = 42;
+            this.butLoad.Text = "Load";
+            this.butLoad.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Author";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Book Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Book ID";
+            // 
+            // textAuthor
+            // 
+            this.textAuthor.Location = new System.Drawing.Point(236, 94);
+            this.textAuthor.Name = "textAuthor";
+            this.textAuthor.Size = new System.Drawing.Size(100, 21);
+            this.textAuthor.TabIndex = 38;
+            // 
+            // textBookName
+            // 
+            this.textBookName.Location = new System.Drawing.Point(236, 60);
+            this.textBookName.Name = "textBookName";
+            this.textBookName.Size = new System.Drawing.Size(219, 21);
+            this.textBookName.TabIndex = 37;
+            // 
+            // textBookID
+            // 
+            this.textBookID.Location = new System.Drawing.Point(236, 30);
+            this.textBookID.Name = "textBookID";
+            this.textBookID.Size = new System.Drawing.Size(100, 21);
+            this.textBookID.TabIndex = 36;
+            // 
+            // Uc_Addbooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textIsbn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textPublisher);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textGenre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.butDelete);
+            this.Controls.Add(this.butInsert);
+            this.Controls.Add(this.butUpdate);
+            this.Controls.Add(this.butLoad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textAuthor);
+            this.Controls.Add(this.textBookName);
+            this.Controls.Add(this.textBookID);
+            this.Name = "Uc_Addbooks";
+            this.Size = new System.Drawing.Size(564, 381);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textIsbn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textPublisher;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textGenre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button butFind;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button butDelete;
+        private System.Windows.Forms.Button butInsert;
+        private System.Windows.Forms.Button butUpdate;
+        private System.Windows.Forms.Button butLoad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textAuthor;
+        private System.Windows.Forms.TextBox textBookName;
+        private System.Windows.Forms.TextBox textBookID;
+    }
+}
