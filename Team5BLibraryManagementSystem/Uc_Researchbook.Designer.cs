27e5bd6cabeeb797bed04b6967189a10e6a62a93
snowzinc <e0321131@u.nus.edu>
2018-09-27 19:53:44 +0800

    edited search books

--- Team5BLibraryManagementSystem/Uc_Researchbook.Designer.cs
+++ Team5BLibraryManagementSystem/Uc_Researchbook.Designer.cs
@@ -28,74 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textSearch = new System.Windows.Forms.TextBox();
-            this.dataRearch = new System.Windows.Forms.DataGridView();
+            this.text_Search = new System.Windows.Forms.TextBox();
+            this.data_Search = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
-            this.listSearch = new System.Windows.Forms.ListBox();
             this.butSearch = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataRearch)).BeginInit();
+            this.comboBox_Choose = new System.Windows.Forms.ComboBox();
+            this.labelSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.data_Search)).BeginInit();
             this.SuspendLayout();
             // 
-            // textSearch
+            // text_Search
             // 
-            this.textSearch.Location = new System.Drawing.Point(293, 71);
-            this.textSearch.Name = "textSearch";
-            this.textSearch.Size = new System.Drawing.Size(100, 21);
-            this.textSearch.TabIndex = 37;
+            this.text_Search.Location = new System.Drawing.Point(220, 71);
+            this.text_Search.Name = "text_Search";
+            this.text_Search.Size = new System.Drawing.Size(190, 21);
+            this.text_Search.TabIndex = 37;
             // 
-            // dataRearch
+            // data_Search
             // 
-            this.dataRearch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataRearch.Location = new System.Drawing.Point(115, 132);
-            this.dataRearch.Name = "dataRearch";
-            this.dataRearch.RowTemplate.Height = 23;
-            this.dataRearch.Size = new System.Drawing.Size(291, 177);
-            this.dataRearch.TabIndex = 36;
-            this.dataRearch.Visible = false;
+            this.data_Search.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_Search.Location = new System.Drawing.Point(119, 121);
+            this.data_Search.Name = "data_Search";
+            this.data_Search.RowTemplate.Height = 23;
+            this.data_Search.Size = new System.Drawing.Size(291, 177);
+            this.data_Search.TabIndex = 36;
+            this.data_Search.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(67, 73);
+            this.label1.Location = new System.Drawing.Point(46, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 35;
             this.label1.Text = "Search By:";
             // 
-            // listSearch
-            // 
-            this.listSearch.FormattingEnabled = true;
-            this.listSearch.ItemHeight = 12;
-            this.listSearch.Items.AddRange(new object[] {
-            "Title",
-            "Book ID",
-            "Author"});
-            this.listSearch.Location = new System.Drawing.Point(154, 73);
-            this.listSearch.Name = "listSearch";
-            this.listSearch.Size = new System.Drawing.Size(120, 16);
-            this.listSearch.TabIndex = 33;
-            // 
             // butSearch
             // 
-            this.butSearch.Location = new System.Drawing.Point(423, 73);
+            this.butSearch.Location = new System.Drawing.Point(428, 71);
             this.butSearch.Name = "butSearch";
             this.butSearch.Size = new System.Drawing.Size(75, 23);
             this.butSearch.TabIndex = 34;
             this.butSearch.Text = "Search";
             this.butSearch.UseVisualStyleBackColor = true;
+            this.butSearch.Click += new System.EventHandler(this.butSearch_Click);
+            // 
+            // comboBox_Choose
+            // 
+            this.comboBox_Choose.FormattingEnabled = true;
+            this.comboBox_Choose.Items.AddRange(new object[] {
+            "Title",
+            "Author",
+            "Book ID",
+            "Publisher",
+            "Category",
+            "Publisher Year",
+            "Isbn",
+            "Status",
+            ""});
+            this.comboBox_Choose.Location = new System.Drawing.Point(122, 71);
+            this.comboBox_Choose.Name = "comboBox_Choose";
+            this.comboBox_Choose.Size = new System.Drawing.Size(92, 20);
+            this.comboBox_Choose.TabIndex = 38;
+            // 
+            // labelSearch
+            // 
+            this.labelSearch.AutoSize = true;
+            this.labelSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSearch.ForeColor = System.Drawing.Color.Crimson;
+            this.labelSearch.Location = new System.Drawing.Point(219, 95);
+            this.labelSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelSearch.Name = "labelSearch";
+            this.labelSearch.Size = new System.Drawing.Size(193, 13);
+            this.labelSearch.TabIndex = 74;
+            this.labelSearch.Text = "*Please key in the search words.";
             // 
             // Uc_Researchbook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textSearch);
-            this.Controls.Add(this.dataRearch);
+            this.Controls.Add(this.labelSearch);
+            this.Controls.Add(this.comboBox_Choose);
+            this.Controls.Add(this.text_Search);
+            this.Controls.Add(this.data_Search);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listSearch);
             this.Controls.Add(this.butSearch);
             this.Name = "Uc_Researchbook";
             this.Size = new System.Drawing.Size(564, 381);
-            ((System.ComponentModel.ISupportInitialize)(this.dataRearch)).EndInit();
+            this.Load += new System.EventHandler(this.Uc_Researchbook_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data_Search)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,10 +124,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textSearch;
-        private System.Windows.Forms.DataGridView dataRearch;
+        private System.Windows.Forms.TextBox text_Search;
+        private System.Windows.Forms.DataGridView data_Search;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listSearch;
         private System.Windows.Forms.Button butSearch;
+        private System.Windows.Forms.ComboBox comboBox_Choose;
+        private System.Windows.Forms.Label labelSearch;
     }
 }
--- Team5BLibraryManagementSystem/Uc_Researchbook.cs
+++ Team5BLibraryManagementSystem/Uc_Researchbook.cs
@@ -16,5 +16,69 @@ namespace Team5BLibraryManagementSystem
         {
             InitializeComponent();
         }
+
+        private void Uc_Researchbook_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butSearch_Click(object sender, EventArgs e)
+        {
+            SA47Team05BESNETLMSEntities context = new SA47Team05BESNETLMSEntities();
+            if (text_Search.Text == "" || comboBox_Choose.SelectedItem == null)
+            {
+                labelSearch.Visible = true;
+            }
+            else
+            {
+                labelSearch.Visible = false;
+                switch (comboBox_Choose.SelectedItem)
+                {
+                    case "Title":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.title.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Author":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.author.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Isbn":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.isbn.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Book ID":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.title.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Status":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.status.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Publisher":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.publisher.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Publisher Year":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.publishyear.ToString() == text_Search.Text).ToList();
+                        break;
+
+                    case "Category":
+                        data_Search.Visible = true;
+                        data_Search.DataSource = context.ViewBooksListings.Where(x => x.category.ToString() == text_Search.Text).ToList();
+                        break;
+                }
+            }
+
+
+
+        }
+          
     }
 }
